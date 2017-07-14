8afb722def2c4e75691e3b368160200ed3dd0258
Evan Sosenko <evan.sosenko@meltwater.com>
2017-07-13 18:17:17 -0700

    Fix check example

--- examples/check.js
+++ examples/check.js
@@ -4,7 +4,8 @@ export default options => async () => {
   const healthCheck = createHealthCheck(async x => {
     await new Promise(resolve => setTimeout(resolve, 300))
     return x
-  }, {cache: null})
+  }, {ttl: 1})
+  await healthCheck(true)
   const healths = [false, true, false, true].map(healthCheck)
   await new Promise(resolve => setTimeout(resolve, 1100))
   return Promise.all([...healths, healthCheck(false)])
