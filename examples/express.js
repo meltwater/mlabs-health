adda2b8976b45dd0913115ca997a455032a4504b
Evan Sosenko <evan.sosenko@meltwater.com>
2017-07-21 14:23:12 -0700

    Add links to express example

--- examples/express.js
+++ examples/express.js
@@ -15,6 +15,7 @@ export default ({log}) => async (availability = 0.8) => {
   )
 
   const app = express()
+
   app.use('/health', expressHealthRouter({
     healthy: createHealthy(),
     healthMonitor,
@@ -26,10 +27,30 @@ export default ({log}) => async (availability = 0.8) => {
     log
   }))
 
+  app.use('/', (req, res, next) => {
+    res.send(`
+      <!doctype html>
+      <html lang="en">
+        <head><meta charset="utf-8"></head>
+        <body>
+          <ul>
+            <li><a href='/status'>/status</a></li>
+            <li><a href='/status/foo'>/status/foo</a></li>
+            <li><a href='/status/bar'>/status/bar</a></li>
+          </ul>
+          <ul>
+            <li><a href='/health'>/health</a></li>
+            <li><a href='/health/foo'>/health/foo</a></li>
+            <li><a href='/health/bar'>/health/bar</a></li>
+          </ul>
+        </body>
+      </html>
+    `)
+  })
+
   const port = 3000
   const host = 'localhost'
   app.listen(port, () => {
-    log.info({port, host}, `Server: http://${host}:${port}/health`)
-    log.info({port, host}, `Server: http://${host}:${port}/status`)
+    log.info({port, host}, `Server: http://${host}:${port}`)
   })
 }
