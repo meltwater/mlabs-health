2a47cd08dbc22f02a82c67e91ec911479e6b4f0d
Evan Sosenko <evan.sosenko@meltwater.com>
2018-02-28 12:49:26 -0800

    Use sleeP in example

--- examples/check.js
+++ examples/check.js
@@ -1,12 +1,14 @@
+import { sleeP } from '@meltwater/phi'
+
 import { createHealthCheck } from '../lib'
 
 export default options => async () => {
   const healthCheck = createHealthCheck(async x => {
-    await new Promise(resolve => setTimeout(resolve, 300))
+    await sleeP(600)
     return x
   }, {ttl: 1})
   await healthCheck(true)
   const healths = [false, true, false, true].map(healthCheck)
-  await new Promise(resolve => setTimeout(resolve, 1100))
+  await sleeP(1100)
   return Promise.all([...healths, healthCheck(false)])
 }
